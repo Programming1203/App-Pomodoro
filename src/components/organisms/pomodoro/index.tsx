1d2a4bd0901466eb8b1c55718160fa339aa4e586
bibsgr <bibeli0802@gmail.com>
2023-11-10 01:42:31 -0400

    fix: correct change background color in whole app

--- src/components/organisms/pomodoro/index.tsx
+++ src/components/organisms/pomodoro/index.tsx
@@ -39,7 +39,7 @@ const Pomodoro: React.FC<PomodoroProps> = ({ onColorChange }) => {
     return () => {
       if (interval) clearInterval(interval);
     };
-  }, [isActive, time, isWorking, onColorChange]);
+  }, [isActive, time, isWorking, onColorChange(colors[currentTime])]);
 
   const handleStartStop = () => {
     setIsActive((prev) => !prev);
